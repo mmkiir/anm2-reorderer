a824a3eb12da431fdad4b12fec4f146475311b63
mmkiir <porasjeet@gmail.com>
2021-08-03 13:51:07 +0530

    feat: add file menu

--- anm2_reorderer/__main__.py
+++ anm2_reorderer/__main__.py
@@ -7,6 +7,32 @@ class MainWindow(wx.Frame):
     def __init__(self, *args, **kw) -> None:
         super().__init__(*args, **kw)
 
+        file_menu: wx.Menu = wx.Menu()
+
+        open_menu_item: wx.MenuItem = wx.MenuItem(
+            file_menu, wx.ID_OPEN, "&Open...")
+        file_menu.Append(open_menu_item)
+
+        save_menu_item: wx.MenuItem = wx.MenuItem(
+            file_menu, wx.ID_SAVE, "&Save")
+        file_menu.Append(save_menu_item)
+
+        saveas_menu_item: wx.MenuItem = wx.MenuItem(
+            file_menu, wx.ID_SAVEAS, "&Save As...")
+        file_menu.Append(saveas_menu_item)
+
+        file_menu.AppendSeparator()
+
+        exit_menu_item: wx.MenuItem = wx.MenuItem(
+            file_menu, wx.ID_EXIT, "Exit")
+        file_menu.Append(exit_menu_item)
+
+        menu_bar: wx.MenuBar = wx.MenuBar()
+
+        menu_bar.Append(file_menu, "&File")
+
+        self.SetMenuBar(menu_bar)
+
 
 app = wx.App()
 
