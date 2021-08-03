4b97ecd290592521a1bcba75efade9e9866f41da
mmkiir <porasjeet@gmail.com>
2021-08-03 16:48:12 +0530

    feat: add list_box and buttons to list and organize animations

--- anm2_reorderer/__main__.py
+++ anm2_reorderer/__main__.py
@@ -2,7 +2,7 @@
 
 import wx
 import xml.etree.ElementTree as ET
-from typing import Any, Optional
+from typing import Any, Optional, cast
 
 
 class MainWindow(wx.Frame):
@@ -11,6 +11,7 @@ class MainWindow(wx.Frame):
 
         self.current_file_path: Optional[str] = None
         self.current_file_root: Optional[ET.ElementTree] = None
+        self.current_file_is_saved: bool = False
 
         file_menu: wx.Menu = wx.Menu()
 
@@ -40,6 +41,46 @@ class MainWindow(wx.Frame):
 
         self.SetMenuBar(menu_bar)
 
+        panel: wx.Panel = wx.Panel(self)
+
+        h_box: wx.BoxSizer = wx.BoxSizer(wx.HORIZONTAL)
+
+        self.list_box: wx.ListBox = wx.ListBox(panel)
+
+        h_box.Add(self.list_box, wx.ID_ANY, wx.EXPAND | wx.ALL, 20)
+
+        button_panel: wx.Panel = wx.Panel(panel)
+
+        v_box: wx.BoxSizer = wx.BoxSizer(wx.VERTICAL)
+
+        move_up_button: wx.Button = wx.Button(
+            button_panel, wx.ID_ANY, 'Move up')
+
+        self.Bind(wx.EVT_BUTTON, self.OnMoveUp, move_up_button)
+
+        move_down_button: wx.Button = wx.Button(
+            button_panel, wx.ID_ANY, 'Move down')
+        self.Bind(wx.EVT_BUTTON, self.OnMoveDown, move_down_button)
+
+        rename_button: wx.Button = wx.Button(
+            button_panel, wx.ID_ANY, 'Rename')
+        self.Bind(wx.EVT_BUTTON, self.OnRename, rename_button)
+
+        delete_button: wx.Button = wx.Button(
+            button_panel, wx.ID_ANY, 'Delete')
+        self.Bind(wx.EVT_BUTTON, self.OnDelete, delete_button)
+
+        v_box.Add(move_up_button, 0, wx.EXPAND | wx.RIGHT)
+        v_box.Add(move_down_button, 0, wx.EXPAND | wx.RIGHT)
+        v_box.Add(rename_button, 0, wx.EXPAND | wx.RIGHT)
+        v_box.Add(delete_button, 0, wx.EXPAND | wx.RIGHT)
+
+        button_panel.SetSizer(v_box)
+
+        h_box.Add(button_panel, 0, wx.EXPAND | wx.RIGHT, 20)
+
+        panel.SetSizer(h_box)
+
     def OnOpen(self, _) -> None:
         with wx.FileDialog(self, "Open", wildcard="ANM2 files (*.anm2)|*.anm2", style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST) as file_dialog:
             if file_dialog.ShowModal() == wx.ID_CANCEL:
@@ -49,12 +90,36 @@ class MainWindow(wx.Frame):
             try:
                 with open(file_path, 'r') as file:
                     self.current_file_path = file_path
+                    self.current_file_is_saved = True
                     self.current_file_root = ET.parse(file_path)
+
+                    animations = self.current_file_root.find('Animations')
+
+                    if not animations:
+                        return
+
+                    animations = cast(ET.Element, animations)
+
+                    self.list_box.InsertItems(
+                        [animation.attrib['Name'] for animation in animations.iter('Animation')], 0)
+
             except IOError:
                 wx.LogError(f"Cannot open file {file_path}")
 
     def OnExit(self, _) -> None:
         self.Close()
+
+    def OnMoveUp(self, _) -> None:
+        pass
+
+    def OnMoveDown(self, _) -> None:
+        pass
+
+    def OnRename(self, _) -> None:
+        pass
+
+    def OnDelete(self, _) -> None:
+        pass
 
 
 app = wx.App()
